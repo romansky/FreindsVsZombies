e950617c3738543664752c77d9b3df58bc9be2c4
Near Privman <near@joinstix.com>
2014-01-24 17:48:32 +0200

    Adjust resolution and skip menu screen

--- js/game.js
+++ js/game.js
@@ -13,7 +13,7 @@ var game = {
     "onload" : function () {
 
         // Initialize the video.
-        if (!me.video.init("screen", 640, 480, true, 'auto')) {
+        if (!me.video.init("screen", 800, 576, true)) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
@@ -49,12 +49,12 @@ var game = {
         me.entityPool.add("mainPlayer", game.PlayerEntity);
 
         // enable the keyboard
-        me.input.bindKey(me.input.KEY.LEFT,  "left");
-        me.input.bindKey(me.input.KEY.RIGHT, "right");
-        me.input.bindKey(me.input.KEY.X,     "jump", true);
+        // me.input.bindKey(me.input.KEY.LEFT,  "left");
+        // me.input.bindKey(me.input.KEY.RIGHT, "right");
+        // me.input.bindKey(me.input.KEY.X,     "jump", true);
 
         // Start the game.
-        me.state.change(me.state.MENU);
+        me.state.change(me.state.PLAY);
     }
 };
 
